bfd716a3c073f101056d796d6a338ef8e78a4649
crane <3376063337@qq.com>
2024-06-25 17:07:32 +0800

    fix

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
 # Zcmu_Score
+
 浙江中医药大学成绩自动查询并邮箱提醒
 
 ## 使用
 
-安装依赖
+1.安装依赖
 
 ```shell
 pip install requests
 pip install yagmail
+```
+
+2.修改配置文件
+
+在`config.json`中填写学号、密码、邮箱smtp服务器、邮箱账号、邮箱授权码、接收邮箱
+
+```json
+{
+    "login": {
+        "username": "2019xxxxxx",
+        "password": "xxxxxxxx"
+    },
+    "smtp_email": {
+        "user": "",
+        "password": "",
+        "host": "",
+        "email_to": [
+            ""
+        ]
+    }
+}
+```
+
+3.运行
+
+```shell
+python Zcmu_Score.py
 ```
--- Zcmu_Score.py
+++ Zcmu_Score.py
@@ -3,14 +3,23 @@ import time
 import json
 import lxml.etree
 import yagmail
+import logging
+import os
 
 '''
-使用之前先填写账号密码，邮箱SMTP服务账号授权码服务器地址，接收方邮箱
+使用之前先在config填写账号密码，邮箱SMTP服务账号授权码服务器地址，接收方邮箱
 '''
+# 配置日志
+logging.basicConfig(filename='score.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 session = requests.Session()
 
+with open('config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
 def login():
+    global session
+
     url="https://cas.paas.zcmu.edu.cn/cas/login?service=https%3A%2F%2Fxjyt.zcmu.edu.cn%3A443%2Fcas%2Flogin%2Fcas%2Flogin%3Frzxx%3Dsupwisdom%26service%3Dhttps%253A%252F%252Fpuser.zcmu.edu.cn%252Fhome-page"
     login_url = "https://cas.paas.zcmu.edu.cn/cas/login?service=https%3A%2F%2Fxjyt.zcmu.edu.cn%3A443%2Fcas%2Flogin%2Fcas%2Flogin%3Frzxx%3Dsupwisdom%26service%3Dhttps%253A%252F%252Fpuser.zcmu.edu.cn%252Fhome-page"
 
@@ -26,8 +35,8 @@ def login():
     session.cookies.update({"Cookie": "SESSION=8b3b1b3b-3b3b-3b3b-3b3b-3b3b3b3b3b3b; Hm_lvt_d605d8df6bf5ca8a54fe078683196518={}; Hm_lpvt_d605d8df6bf5ca8a54fe078683196518={}".format(str(int(time.time())-10), str(int(time.time()))),})   
 
     data = {
-        "username": "", #账号
-        "password": "", #密码
+        "username": config["login"]["username"],
+        "password": config["login"]["password"],
         "captcha": "",
         "currentMenu": "1",
         "failN": "0",
@@ -49,10 +58,10 @@ def login():
 def send_email(content):
     # 连接服务器
     # 用户名、授权码、服务器地址
-    yag_server = yagmail.SMTP(user='', password='', host='smtp.qq.com')
+    yag_server = yagmail.SMTP(user=config["smtp_email"]["user"], password=config["smtp_email"]["password"], host=config["smtp_email"]["host"])
 
     # 发送对象列表
-    email_to = ['..@qq.com',]
+    email_to = config["smtp_email"]["email_to"]
     email_title = content
     email_content = content
     # # 附件列表
@@ -64,46 +73,55 @@ def send_email(content):
     # 关闭连接
     yag_server.close()
 
-cj_url = "https://jwmk.zcmu.edu.cn/jwglxt/cjjfgl/xkjsjfsqsh_cxKcxxView.html?doType=query&xnm=2023&xqm=12&queryModel.showCount=999&queryModel.pageSize=999"
+    print(f"Email sent with content: {content}")
+    logging.info(f"Email sent with content: {content}")
 
-login()
 
-kc={}
+def query_scores():
+    cj_url = "https://jwmk.zcmu.edu.cn/jwglxt/cjjfgl/xkjsjfsqsh_cxKcxxView.html?doType=query&xnm=2023&xqm=12&queryModel.showCount=999&queryModel.pageSize=999"
+    global session
+    login()
 
-try:
-    while True:
-        response = session.get(cj_url,verify=False)
-        if "学教一体化平台" in response.text:
-            session = requests.Session()
-            login()
-            print("登录过期，重新登录")
-        else:
-            response = response.json()
-            for i in response["items"]:
-                if i["kcmc"] not in kc:
-                    content=''
-                    kc[i["kcmc"]] = i["cj"]
-                    content = i["kcmc"] + " " + i["cj"]
-                    send_email(content)
-                print(i["kcmc"],i["cj"],"查询时间：",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        time.sleep(60)
-except Exception as e:
-    print(e)
-    send_email(str(e))
+    kc={}
+    if os.path.exists('kc.json'):
+        with open('kc.json', 'r', encoding='utf-8') as f:
+            kc = json.load(f)
 
-# while True:
-#     response = session.get(cj_url,verify=False)
-#     if "学教一体化平台" in response.text:
-#         session = requests.Session()
-#         login()
-#         print("登录过期，重新登录")
-#     else:
-#         response = response.json()
-#         for i in response["items"]:
-#             if i["kcmc"] not in kc:
-#                 content=''
-#                 kc[i["kcmc"]] = i["cj"]
-#                 content = i["kcmc"] + " " + i["cj"]
-#                 send_email(content)
-#             print(i["kcmc"],i["cj"],"查询时间：",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-#     time.sleep(60)
+    try:
+        while True:
+            response = session.get(cj_url,verify=False)
+            if "学教一体化平台" in response.text:
+                session = requests.Session()
+                login()
+                print("登陆过期，重新登录","时间：",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                logging.error("登陆过期，重新登录")
+                continue
+            elif "学教一体化" in response.text:
+                session = requests.Session()
+                login()
+                print("异地登陆，重新登录","时间：",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                logging.error("异地登陆，重新登录")
+                continue
+            else:
+                response = response.json()
+                for i in response["items"]:
+                    if i["kcmc"] not in kc:
+                        kc[i["kcmc"]] = i["cj"]
+                        content = i["kcmc"] + " " + i["cj"]
+                        send_email(content)
+                        with open('kc.json', 'w', encoding='utf-8') as f:
+                            json.dump(kc, f, ensure_ascii=False, indent=4)
+                    print(i["kcmc"],i["cj"],"查询时间：",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                    logging.info(f"{i['kcmc']} {i['cj']}")
+            time.sleep(60)
+    except Exception as e:
+        print(e)
+        logging.error(e)
+        send_email(str(e))
+
+
+query_scores()
+session = requests.Session()
+query_scores()
+send_email("重新启动失败")
+    
